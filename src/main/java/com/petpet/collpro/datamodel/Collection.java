dbe83af783bf5bba2b180adfd37275fb0ab2ab4c
Petar Petrov <pepetrov@gmail.com>
2011-05-06 12:43:02 +0200

    adding entities

--- src/main/java/com/petpet/collpro/datamodel/Collection.java
+++ src/main/java/com/petpet/collpro/datamodel/Collection.java
@@ -1,5 +1,58 @@
 package com.petpet.collpro.datamodel;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 public class Collection {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    
+    @Basic
+    @Column(name = "NAME", nullable = false)
+    private String name;
+    
+    @OneToMany(mappedBy = "collection")
+    private Set<Element> elements;
+    
+    public Collection() {
+        this.elements = new HashSet<Element>();
+    }
+
+    public Collection(String name) {
+        this();
+        this.setName(name);
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setElements(Set<Element> elements) {
+        this.elements = elements;
+    }
+
+    public Set<Element> getElements() {
+        return elements;
+    }
 }
--- src/main/java/com/petpet/collpro/datamodel/Element.java
+++ src/main/java/com/petpet/collpro/datamodel/Element.java
@@ -1,13 +1,64 @@
 package com.petpet.collpro.datamodel;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Element {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    
+    @Basic
+    @Column(name = "NAME", nullable = false)
     private String name;
+    
+    @Basic
+    @Column(name = "FILEPATH", nullable = false)
+    private String filepath;
+    
+    @ManyToOne
+    private Collection collection;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Set<Characteristic<?>> info;
+    
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Set<Characteristic<?>> identification;
+    
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Set<Characteristic<?>> metadata;
+    
+    public Element() {
+        this.setInfo(new HashSet<Characteristic<?>>());
+        this.setIdentification(new HashSet<Characteristic<?>>());
+        this.setMetadata(new HashSet<Characteristic<?>>());
+    }
+    
+    public Element(String name, String filepath) {
+        this();
+        this.setName(name);
+        this.setFilepath(filepath);
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -15,6 +66,46 @@ public class Element {
 
     public String getName() {
         return name;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setCollection(Collection collection) {
+        this.collection = collection;
+    }
+
+    public Collection getCollection() {
+        return collection;
+    }
+
+    public void setInfo(Set<Characteristic<?>> info) {
+        this.info = info;
+    }
+
+    public Set<Characteristic<?>> getInfo() {
+        return info;
+    }
+
+    public void setIdentification(Set<Characteristic<?>> identification) {
+        this.identification = identification;
+    }
+
+    public Set<Characteristic<?>> getIdentification() {
+        return identification;
+    }
+
+    public void setMetadata(Set<Characteristic<?>> metadata) {
+        this.metadata = metadata;
+    }
+
+    public Set<Characteristic<?>> getMetadata() {
+        return metadata;
     }
     
     
