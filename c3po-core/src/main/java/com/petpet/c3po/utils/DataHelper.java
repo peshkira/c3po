3a0eb9934bf5f02d2b1701198952e89d4d997804
Petar Petrov <me@petarpetrov.org>
2012-05-29 11:21:50 +0200

    adding better pattern matching for dates

--- c3po-core/src/main/java/com/petpet/c3po/utils/DataHelper.java
+++ c3po-core/src/main/java/com/petpet/c3po/utils/DataHelper.java
@@ -2,6 +2,7 @@ package com.petpet.c3po.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -25,6 +26,9 @@ public final class DataHelper {
 
   private static Properties TYPES;
 
+  private static final String[] PATTERNS = { "yyyy:MM:dd HH:mm:ss", "MM/dd/yyyy HH:mm:ss", "dd MMM yyyy HH:mm",
+      "EEE dd MMM yyyy HH:mm", "EEE, MMM dd, yyyy hh:mm:ss a", "EEE, MMM dd, yyyy hh:mm a", "EEE dd MMM yyyy HH.mm", "HH:mm MM/dd/yyyy" };
+
   public static void init() {
     try {
       InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("datatypes.properties");
@@ -33,89 +37,6 @@ public final class DataHelper {
       in.close();
     } catch (IOException e) {
       e.printStackTrace();
-    }
-  }
-  
-  public static String getPropertyType(String key) {
-    return TYPES.getProperty(key, "STRING");
-  }
-
-  public static Object getTypedValue(String t, String value) {
-
-    if (value == null) {
-      return "";
-    }
-
-    PropertyType type = PropertyType.valueOf(t);
-    Object result = null;
-    switch (type) {
-      case BOOL:
-        result = DataHelper.getBooleanValue(value);
-        break;
-      case INTEGER:
-        result = DataHelper.getIntegerValue(value);
-        break;
-      case FLOAT:
-        result = DataHelper.getDoubleValue(value);
-        break;
-      case DATE:
-        result = DataHelper.getDateValue(value);
-        break;
-      case ARRAY:
-        break;
-    }
-
-    return (result == null) ? value : result;
-
-  }
-
-  public static Date getDateValue(String value) {
-    LOG.trace("parsing value {} as date", value);
-    
-    try {
-      return new Date(Long.valueOf(value));
-
-    } catch (NumberFormatException e) {
-      LOG.debug("date is not in long representation, trying pattern matching: {}", e.getMessage());
-
-      final SimpleDateFormat format = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
-
-      try {
-        return format.parse(value);
-
-      } catch (ParseException pe) {
-        LOG.info("date could not be parsed: {}", pe.getMessage());
-        return null;
-      }
-    }
-  }
-
-  private static Double getDoubleValue(String value) {
-    try {
-      return Double.parseDouble(value);
-    } catch (NumberFormatException e) {
-      LOG.warn("Value {} is not an float", value);
-      return null;
-    }
-  }
-
-  private static Integer getIntegerValue(String value) {
-    try {
-      return Integer.parseInt(value);
-    } catch (NumberFormatException e) {
-      LOG.warn("Value {} is not an integer", value);
-      return null;
-    }
-  }
-
-  private static Boolean getBooleanValue(String value) {
-    if (value.equalsIgnoreCase("yes") || value.equalsIgnoreCase("true")) {
-      return new Boolean(true);
-    } else if (value.equalsIgnoreCase("no") || value.equalsIgnoreCase("false")) {
-      return new Boolean(false);
-    } else {
-      LOG.warn("Value {} is not a boolean", value);
-      return null;
     }
   }
 
@@ -154,11 +75,110 @@ public final class DataHelper {
     final BasicDBObject property = new BasicDBObject();
     property.put("_id", p.getId());
     property.put("key", p.getKey());
-    // property.put("name", p.);
     property.put("type", p.getType());
-    // property.put("description", this.description);
 
     return property;
+  }
+
+  public static String getPropertyType(String key) {
+    return TYPES.getProperty(key, "STRING");
+  }
+
+  public static Object getTypedValue(String t, String value) {
+
+    if (value == null) {
+      return "";
+    }
+
+    PropertyType type = PropertyType.valueOf(t);
+    Object result = null;
+    switch (type) {
+      case BOOL:
+        result = DataHelper.getBooleanValue(value);
+        break;
+      case INTEGER:
+        result = DataHelper.getIntegerValue(value);
+        break;
+      case FLOAT:
+        result = DataHelper.getDoubleValue(value);
+        break;
+      case DATE:
+        result = DataHelper.getDateValue(value);
+        break;
+      case ARRAY:
+        break;
+    }
+
+    return (result == null) ? value : result;
+
+  }
+
+  public static Date getDateValue(String value) {
+    LOG.trace("parsing value {} as date", value);
+
+    try {
+      return new Date(Long.valueOf(value));
+
+    } catch (NumberFormatException e) {
+      LOG.trace("date is not in long representation, trying pattern matching: {}", e.getMessage());
+
+      final SimpleDateFormat fmt = new SimpleDateFormat();
+
+      Date result = null;
+      for (String p : PATTERNS) {
+
+        fmt.applyPattern(p);
+        result = DataHelper.parseDate(fmt, value);
+
+        if (result != null) {
+          break;
+        }
+      }
+
+      if (result == null) {
+        LOG.debug("No pattern matching for value {}", value);
+      }
+
+      return result;
+    }
+  }
+
+  private static Double getDoubleValue(String value) {
+    try {
+      return Double.parseDouble(value);
+    } catch (NumberFormatException e) {
+      LOG.warn("Value {} is not an float", value);
+      return null;
+    }
+  }
+
+  private static Integer getIntegerValue(String value) {
+    try {
+      return Integer.parseInt(value);
+    } catch (NumberFormatException e) {
+      LOG.warn("Value {} is not an integer", value);
+      return null;
+    }
+  }
+
+  private static Boolean getBooleanValue(String value) {
+    if (value.equalsIgnoreCase("yes") || value.equalsIgnoreCase("true")) {
+      return new Boolean(true);
+    } else if (value.equalsIgnoreCase("no") || value.equalsIgnoreCase("false")) {
+      return new Boolean(false);
+    } else {
+      LOG.warn("Value {} is not a boolean", value);
+      return null;
+    }
+  }
+
+  private static Date parseDate(DateFormat fmt, String d) {
+    try {
+      return fmt.parse(d);
+    } catch (ParseException e) {
+      LOG.trace("date could not be parsed: {}", e.getMessage());
+      return null;
+    }
   }
 
   private DataHelper() {
