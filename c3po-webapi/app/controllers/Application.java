6549e766ca498d8e893be4329976ac575cee1b2b
Petar Petrov <me@petarpetrov.org>
2012-06-22 20:26:30 +0200

    adding new menu and filtering options elements

--- c3po-webapi/app/controllers/Application.java
+++ c3po-webapi/app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.Collections;
 import java.util.List;
 
+import com.avaje.ebean.validation.AssertFalse;
 import com.petpet.c3po.api.dao.PersistenceLayer;
 import com.petpet.c3po.common.Constants;
 import com.petpet.c3po.utils.Configurator;
@@ -15,8 +16,14 @@ public class Application extends Controller {
 
   public static Result index() {
     return ok(
-        index.render("c3po")
+        index.render("c3po", getCollectionNames())
         );
+  }
+  
+  public static Result setCollection(String c) {
+    System.out.println("Received collection setup change for " + c);
+    session().put("current.collection", c);
+    return ok("The collection was changed successfully");
   }
   
   public static List<String> getCollectionNames() {
