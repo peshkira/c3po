b45d20f99e96d52ec4bd91555900693de1137c4c
artourkin <artur.kulmukhametov@gmail.com>
2014-03-11 13:24:46 +0100

    proper rules edition

--- c3po-core/src/main/java/com/petpet/c3po/adaptor/rules/drools/DroolsResolutionWorkerFactory.java
+++ c3po-core/src/main/java/com/petpet/c3po/adaptor/rules/drools/DroolsResolutionWorkerFactory.java
@@ -1,74 +1,98 @@
 package com.petpet.c3po.adaptor.rules.drools;
 
-import java.io.File;
-import java.util.LinkedList;
-import java.util.List;
-
+import com.petpet.c3po.utils.C3POFileUtils;
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
 import org.drools.io.ResourceFactory;
+import sun.misc.Launcher;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DroolsResolutionWorkerFactory {
 
-  /**
-   * The {@link KnowledgeBase} holding all compiled rules.
-   */
-  private KnowledgeBase kbase;
+    /**
+     * The {@link KnowledgeBase} holding all compiled rules.
+     */
+    private KnowledgeBase kbase;
+    private boolean rulesAvailable;
 
-  public DroolsResolutionWorkerFactory() {
-  }
+    public DroolsResolutionWorkerFactory(String pathToRules) {
 
-  public DroolsResolutionWorker createWorker() {
-    DroolsResolutionWorker worker = new DroolsResolutionWorker(this.kbase);
-    return worker;
-  }
+        initKnowledgeBase(pathToRules);
+    }
 
-  public void setSource(File source) {
-    List<File> filenames = new LinkedList<File>();
 
-    if (source.isDirectory()) {
-      for (File file : source.listFiles()) {
-        if (file.isFile() && file.getName().endsWith(".drl")) {
-          filenames.add(file);
+    public DroolsResolutionWorker createWorker() {
+        DroolsResolutionWorker worker = new DroolsResolutionWorker(this.kbase);
+        return worker;
+    }
+
+    private void initKnowledgeBase(String pathToRules) {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        List<File> sources=readRules(pathToRules);
+        for (File filename : sources) {
+            kbuilder.add(ResourceFactory.newFileResource(filename), ResourceType.DRL);
+            System.out.println("Adding rule: " + filename.getPath());
         }
-      }
-    } else if (source.isFile() && source.getName().endsWith(".drl")) {
-      filenames.add(source);
-    }
 
-    this.initKnowledgeBase(filenames);
-  }
-
-    public void setSource(List<File> sources) {
-        this.initKnowledgeBase(sources);
-    }
-
-  private void initKnowledgeBase(List<File> sources) {
-    KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-
-    // load basic ruleset
-    kbuilder.add(ResourceFactory.newClassPathResource(
-        "/rules/conflictResolutionBasicRules.drl", this.getClass()),
-        ResourceType.DRL);
-
-    // load additional rules
-    for (File filename : sources) {
-      kbuilder.add(ResourceFactory.newFileResource(filename), ResourceType.DRL);
-    }
-
-    if (kbuilder.hasErrors()) {
+        if (kbuilder.hasErrors()) {
       /* TODO: proper handling/logging of errors! */
-      System.err.println(kbuilder.getErrors().toString());
+            System.err.println(kbuilder.getErrors().toString());
+        }
+
+        this.kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        this.kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        if (sources.isEmpty()){
+            System.out.println("No conflict reduction rules found in 'rules' folder. The process will continue.");
+            rulesAvailable=false;
+            return;
+        }
+        rulesAvailable=true;
+        RuleActivationListener.getInstance().initialize(
+                this.kbase.getKnowledgePackages());
     }
 
-    this.kbase = KnowledgeBaseFactory.newKnowledgeBase();
-    this.kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
-
-    RuleActivationListener.getInstance().initialize(
-        this.kbase.getKnowledgePackages());
-  }
+    private List<File> readRules(String pathToRules){
+        List<File> result= new ArrayList<File>();
+        final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+        File rulesDir=new File(pathToRules);
+        if (rulesDir.exists())
+        {
+            if (rulesDir.isDirectory()) {
+                List<File> tmp_files= C3POFileUtils.traverseDirectory(rulesDir);
+                for (File file : tmp_files) {
+                    if (file.isFile() && file.getName().endsWith(".drl")) {
+                        result.add(file);
+                    }
+                }
+            } else if (rulesDir.isFile() && rulesDir.getName().endsWith(".drl")) {
+                result.add(rulesDir);
+            }
+        }
+        else if (!jarFile.isFile())
+        {
+            final URL url = Launcher.class.getResource("/" + pathToRules);
+            if (url != null) {
+                try {
+                    final File directory = new File(url.toURI());
+                    result.addAll(C3POFileUtils.traverseDirectory(directory));
+                } catch (URISyntaxException ex) {
+                    // never happens
+                }
+            }
+        }
+        return result;
+    }
+    public boolean isRulesAvailable()
+    {
+        return rulesAvailable;
+    }
 
 }
