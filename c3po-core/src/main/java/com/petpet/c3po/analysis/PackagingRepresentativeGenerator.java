09bb3ba8bec03c467d2a94cd13094790006bd0cf
artur <artur.kulmukhametov@gmail.com>
2015-03-06 15:51:55 +0100

    Packaging algorithm class created

---
+++ c3po-core/src/main/java/com/petpet/c3po/analysis/PackagingRepresentativeGenerator.java
@@ -0,0 +1,23 @@
+package com.petpet.c3po.analysis;
+
+import java.util.List;
+
+/**
+ * Created by artur on 06.03.15.
+ */
+public class PackagingRepresentativeGenerator extends RepresentativeGenerator {
+    @Override
+    public List<String> execute() {
+        return null;
+    }
+
+    @Override
+    public List<String> execute(int limit) {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+}
