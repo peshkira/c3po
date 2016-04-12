b0bb25aec46262e0fbc9ad45d60e42a14eca79c7
artourkin <artur.kulmukhametov@tuwien.ac.at>
2016-04-12 16:46:33 +0200

    This commit is dedicated to the issue #13.

--- c3po-webapi/app/controllers/Samples.java
+++ c3po-webapi/app/controllers/Samples.java
@@ -32,8 +32,7 @@ import views.html.samples;
 import com.petpet.c3po.analysis.RepresentativeAlgorithmFactory;
 import com.petpet.c3po.analysis.RepresentativeGenerator;
 import com.petpet.c3po.api.dao.PersistenceLayer;
-//import com.petpet.c3po.datamodel.Element;
-//import com.petpet.c3po.datamodel.Filter;
+
 import com.petpet.c3po.utils.Configurator;
 
 
@@ -50,7 +49,6 @@ public class Samples extends Controller {
 
   public static Result getSamples(String alg, int size, String props) {
 	Logger.debug("Received a getSamples call, sampling with alg " + alg + " size " + size + " props " + props);
-    //final Configurator configurator = Configurator.getDefaultConfigurator();
     final PersistenceLayer persistence = Configurator.getDefaultConfigurator().getPersistence();
     final Filter filter = Filters.getFilterFromSession();
     final RepresentativeGenerator sg = new RepresentativeAlgorithmFactory().getAlgorithm(alg);
@@ -73,79 +71,13 @@ public class Samples extends Controller {
     	  Iterator<Element> iter = persistence.find( Element.class, idFilter );
     	  samples.add(iter.next());
       }
-     /* BasicDBObject query = new BasicDBObject("uid", new BasicDBObject("$in", output));
-      //DBCursor cursor = pl.find(Constants.TBL_ELEMENTS, query);
-      Filter idFilter = new Filter();
-      idFilter.addFilterCondition( new FilterCondition( new FilterCondition("_id", new ObjectId(id) ) ) );
-      
-      for ( String id : output ) {
-	      idFilter.addFilterCondition( new FilterCondition( "uid", id ) );
-	    }
-      
-      Iterator<Element> iter = pl.find( Element.class, idFilter );
-      
-      while (iter.hasNext()) {
-        samples.add(iter.next());
-      }*/
     }
 
-    return ok(play.libs.Json.toJson(samples));
+
+	  return ok(play.libs.Json.toJson(samples));
   }
   
-  
- /*
-  
-  private static Result indexAsHtml() {
-	    Map<String, String[]> queryString = request().queryString();
-	    Map<String, String[]> query = new HashMap<String, String[]>();
-	    query.putAll( queryString );
 
-	    String[] algArray = query.remove( "alg" );
-	    if ( algArray == null || algArray.length == 0 ) {
-	      return ok( samples.render( null, null ) );
-	    }
-
-	    String alg = algArray[0];
-
-	    if ( !RepresentativeAlgorithmFactory.isValidAlgorithm( alg ) ) {
-	      return ok( samples.render( null,
-	          "The provided algorithm is not supported. Use one of 'sizesampling', 'syssampling', 'distsampling'" ) );
-	    }
-
-	    String[] countArray = query.remove( "count" );
-	    if ( countArray == null || countArray.length == 0 ) {
-	      return ok( samples.render( null, "No count parameter provided" ) );
-	    }
-	    int size = -1;
-	    try {
-	      size = Integer.valueOf( countArray[0] );
-	    } catch ( NumberFormatException e ) {
-	      return ok( samples.render( null, "The provided count parameter is invalid. Please provide a positive integer" ) );
-	    }
-
-	    if ( size <= 0 ) {
-	      return ok( samples.render( null, "The provided count parameter is invalid. Please provide a positive integer" ) );
-	    }
-
-	    RepresentativeGenerator gen = new
-	        RepresentativeAlgorithmFactory().getAlgorithm( alg );
-
-	    if ( alg.equals( "distsampling" ) ) {
-	      String[] propertyArray = query.remove( "property" );
-	      if ( propertyArray == null || propertyArray.length == 0 ) {
-	        return ok( samples.render( null, "Cannot use distsampling without definining at least one property" ) );
-	      }
-
-	      final HashMap<String, Object> options = new HashMap<String, Object>();
-	      options.put( "properties", Arrays.asList( propertyArray ) );
-	      gen.setOptions( options );
-	    }
-
-	    List<Element> smpls = getSamples( gen, query, size );
-	    return ok( samples.render( smpls, null ) );
-
-	  }
-*/
 	  public static Result indexAsJson() {
 	    response().setContentType( "application/json" );
 	    Map<String, String[]> queryString = request().queryString();
