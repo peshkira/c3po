59ecdb2e8dd88eeeee370a6086a2b74f5114cea0
artourkin <artur.kulmukhametov@tuwien.ac.at>
2016-04-20 18:28:41 +0200

    Restructured the conflict overview table.

--- c3po-core/src/test/java/com/petpet/c3po/dao/mapReduceTest.java
+++ c3po-core/src/test/java/com/petpet/c3po/dao/mapReduceTest.java
@@ -40,7 +40,7 @@ public class mapReduceTest {
 
     MongoPersistenceLayer pLayer;
 
-    //@Before
+    @Before
     public void setup() {
         pLayer = new MongoPersistenceLayer();
 
@@ -53,26 +53,56 @@ public class mapReduceTest {
 
         try {
             pLayer.establishConnection(config);
-
         } catch (C3POPersistenceException e) {
             LOG.warn("Could not establish a connection to the persistence layer. All tests will be skipped");
         }
-
     }
 
-
-    //@Test
+    @Test
     public void mapReduceGenericTest() throws Exception {
 
-        String map2 = "function map() {" +
-                        "for (var property in this) {" +
-                            "for (var property2 in this) {" +
-                                "if ( property != null && property2 !=null && property != property2 && this[property].status != null && this[property2].status != null && this[property].status == 'CONFLICT' && this[property2].status == 'CONFLICT') {" +
-                                    "emit([property,this[property].values,this[property].sources, property2,this[property2].values,this[property2].sources],1);"+
-                                "}" +
-                            "}" +
-                       "}" +
-                    "}";
+        String map2 = "function map() {\n" +
+                "\t\tvar result={};\n" +
+                "\t\tif ( this['format'] != null && this['format'].status != null && \n" +
+                "\t\t\tthis['mimetype'] !=null && this['mimetype'].status != null && \n" +
+                "\t\t\tthis['format_version'] !=null && this['format_version'].status != null &&\n" +
+                "\t\t\t(this['format'].status == 'CONFLICT' || this['format_version'].status == 'CONFLICT' || this['mimetype'].status == 'CONFLICT')) {\n" +
+                "\t\t\t\n" +
+                "\t\t\t\tvar format={};\n" +
+                "\t\t\t\tformat.status=this['format'].status;\n" +
+                "\t\t\t\tif (this['format'].status == 'CONFLICT'){\n" +
+                "\t\t\t\t\tformat.values=this['format'].values;\n" +
+                "\t\t\t\t} else{\n" +
+                "\t\t\t\t\tformat.values=[this['format'].value];\n" +
+                "\t\t\t\t}\n" +
+                "\t\t\t\tformat.sources=this['format'].sources;\n" +
+                "\t\t\t\tresult.format=format;\n" +
+                "\n" +
+                "\t\t\t\tvar format_version={};\n" +
+                "\t\t\t\tformat_version.status=this['format_version'].status;\n" +
+                "\t\t\t\tif (this['format_version'].status == 'CONFLICT'){\n" +
+                "\t\t\t\t\tformat_version.values=this['format_version'].values;\n" +
+                "\t\t\t\t} else{\n" +
+                "\t\t\t\t\tformat_version.values=[this['format_version'].value];\n" +
+                "\t\t\t\t}\n" +
+                "\t\t\t\tformat_version.sources=this['format_version'].sources;\n" +
+                "\t\t\t\tresult.format_version=format_version;\n" +
+                "\n" +
+                "\t\t\t\tvar mimetype={};\n" +
+                "\t\t\t\tmimetype.status=this['mimetype'].status;\n" +
+                "\t\t\t\tif (this['mimetype'].status == 'CONFLICT'){\n" +
+                "\t\t\t\t\tmimetype.values=this['mimetype'].values;\n" +
+                "\t\t\t\t} else{\n" +
+                "\t\t\t\t\tmimetype.values=[this['mimetype'].value];\n" +
+                "\t\t\t\t}\n" +
+                "\t\t\t\tmimetype.sources=this['mimetype'].sources;\n" +
+                "\t\t\t\tresult.mimetype=mimetype;\n" +
+                "\t\t}\n" +
+                "\t    if (result!=null)  {\n" +
+                "\t    \temit(result,1);\n" +
+                "\t    }    \n" +
+                "    }\t";
+
         String reduce = "function reduce(key, values) {" +
                 "var res = 0;" +
                 "values.forEach(function (v) {" +
@@ -96,19 +126,17 @@ public class mapReduceTest {
             Source next = sourceIterator.next();
             sources.add(next.getName()+":"+next.getVersion());
         }
-        header+="Count;Property;";
+        header+="Count;";
         for (String source: sources)
-            header+=source+";";
+            header+="Format-"+source+";";
 
-
-
-        header+="Property;";
         for (String source: sources)
-            header+=source+";";
+            header+="FormatVersion-"+source+";";
 
-        header+="URL";
+        for (String source: sources)
+            header+="Mimetype-"+source+";";
 
-
+        header+="SampleURL;Query";
 
         List<BasicDBObject> basicDBObjects = pLayer.mapReduce(map2, reduce, null);
 
@@ -130,48 +158,58 @@ public class mapReduceTest {
             String output="";
             Double count =  obj.getDouble("value");
             output+=count.intValue();
-            BasicDBList id = (BasicDBList) obj.get("_id");
-            String prop1 = id.get(0).toString();
-            output+=";"+prop1;
-            BasicDBList l1=(BasicDBList) (id.get(1));
-            BasicDBList s1=(BasicDBList) (id.get(2));
-            output+=basicDBListsToCSV(l1,s1,sources);
-            /*for (Object o : l1){
-                if (o.toString().length() >50)
-                    continue;
-                output+=","+o.toString();
-            }
-            BasicDBList s1=(BasicDBList) (id.get(2));
-            for (Object o : s1){
-                output+=","+o.toString();
-            }*/
-            String prop2 = id.get(3).toString();
-            output+=";"+prop2;
+            BasicDBObject id1 = (BasicDBObject) obj.get("_id");
+            if (id1.size()==0)  continue;
+            //TODO: finish serializing this shiiit!;
+            BasicDBObject format =(BasicDBObject) id1.get("format");
+            BasicDBObject format_version =(BasicDBObject) id1.get("format_version");
+            BasicDBObject mimetype =(BasicDBObject) id1.get("mimetype");
 
-            BasicDBList l2=(BasicDBList) (id.get(4));
-            BasicDBList s2=(BasicDBList) (id.get(5));
-            output+=basicDBListsToCSV(l2,s2,sources);
-           /* BasicDBList l2=(BasicDBList) (id.get(4));
-            for (Object o : l2){
-                if (o.toString().length() >50)
-                    continue;
-                output+=","+o.toString();
+            BasicDBList format_values = (BasicDBList) format.get("values");
+            BasicDBList format_sources = (BasicDBList) format.get("sources");
+            output+=basicDBListsToCSV(format_values,format_sources,sources);
+
+            BasicDBList format_version_values = (BasicDBList) format_version.get("values");
+            BasicDBList format_version_sources = (BasicDBList) format_version.get("sources");
+            output+=basicDBListsToCSV(format_version_values,format_version_sources,sources);
+
+            BasicDBList mimetype_values = (BasicDBList) mimetype.get("values");
+            BasicDBList mimetype_sources = (BasicDBList) mimetype.get("sources");
+            output+=basicDBListsToCSV(mimetype_values,mimetype_sources,sources);
+
+            BasicDBList andQuery=new BasicDBList();
+            BasicDBObject query;
+            query=new BasicDBObject();
+            if (format.getString("status").equals("CONFLICT")){
+                query.put("format.values", format_values);
+            } else{
+                query.put("format.value", format_values.get(0));
             }
-            BasicDBList s2=(BasicDBList) (id.get(5));
-            for (Object o : s2){
-                output+=","+o.toString();
-            }*/
-            BasicDBObject query=new BasicDBObject();
-            query.put(prop1+".values", l1);
-            query.append(prop2+".values", l2);
+            andQuery.add(query);
+            query=new BasicDBObject();
+            if (format_version.getString("status").equals("CONFLICT")){
+                query.put("format_version.values", format_version_values);
+            } else{
+                query.put("format_version.value", format_version_values.get(0));
+            }
+            andQuery.add(query);
+            query=new BasicDBObject();
+            if (mimetype.getString("status").equals("CONFLICT")){
+                query.put("mimetype.values", mimetype_values);
+            } else{
+                query.put("mimetype.value", mimetype.get(0));
+            }
+            andQuery.add(query);
+            query=new BasicDBObject("$and", andQuery);
             String s = query.toString();
             Iterator<Element> elementIterator = pLayer.findQ(Element.class, query);
+            output+=";";
             if (elementIterator.hasNext()){
                 Element next = elementIterator.next();
-                output+=";" + "http://localhost:9000/c3po/objects/"+ next.getId();
+                output+= "http://localhost:9000/c3po/objects/"+ next.getId();
             }
+            output+=";"+s;
             out.println(output);
-            //  System.out.println(output);
         }
     }
 
@@ -182,18 +220,23 @@ public class mapReduceTest {
             result[i]="";
 
         int size = value.size();
-
-        for (int i=0; i< value.size();i++){
-            String valueConflicted = value.get(i).toString();
-            Integer sourceID = Integer.parseInt( source.get(i).toString());
-            result[sourceID]=valueConflicted;
+        if (size>1) {
+            for (int i = 0; i < value.size(); i++) {
+                String valueConflicted = value.get(i).toString();
+                Integer sourceID = Integer.parseInt(source.get(i).toString());
+                result[sourceID] = valueConflicted;
+            }
+        } else
+        {
+            String valueConflicted = value.get(0).toString();
+            for (int i=0; i< source.size(); i++){
+                Integer sourceID = Integer.parseInt(source.get(i).toString());
+                result[sourceID] = valueConflicted;
+            }
         }
         String output="";
         for (int i=0; i< result.length; i++)
             output += ";"+result[i];
         return output;
     }
-
-
-
 }
